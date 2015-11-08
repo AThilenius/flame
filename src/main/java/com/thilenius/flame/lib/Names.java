bbeafb34bf770e0336c341b6ff1a64faba3c3304
Cooper Benson <skycoop@gmail.com>
2015-11-08 02:16:51 -0700

    Some utility stuff
    Added log helper and classes to keep track of names that can change
    often

---
+++ src/main/java/com/thilenius/flame/lib/Names.java
@@ -0,0 +1,14 @@
+package com.thilenius.flame.lib;
+
+/**
+ * Created by skyco on 11/8/2015.
+ */
+public final class Names
+{
+    public static final class Items
+    {
+        public static final String WOOD_SPARK = "woodenSpark";
+        public static final String CORE = "singularityCore";
+        public static final String PAD = "teleportPad";
+    }
+}
