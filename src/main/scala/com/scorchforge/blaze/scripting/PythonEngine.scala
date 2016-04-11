92dd644593096f79d031e0c1644e7be2653b9999
Cooper Benson <skycoop@gmail.com>
2016-04-10 20:34:33 -0600

    Slight tweak because I'm an idiot

--- src/main/scala/com/scorchforge/blaze/scripting/PythonEngine.scala
+++ src/main/scala/com/scorchforge/blaze/scripting/PythonEngine.scala
@@ -1,8 +1,5 @@
 package com.scorchforge.blaze.scripting
 
-import javax.script.{ScriptEngineManager, ScriptEngine}
-
 class PythonEngine extends ScriptingEngine {
-  protected val scriptEngineManager = new ScriptEngineManager()
   protected val langEngine = scriptEngineManager.getEngineByName("python")
 }
--- src/main/scala/com/scorchforge/blaze/scripting/ScriptingEngine.scala
+++ src/main/scala/com/scorchforge/blaze/scripting/ScriptingEngine.scala
@@ -3,7 +3,7 @@ package com.scorchforge.blaze.scripting
 import javax.script.{Invocable, ScriptEngineManager, ScriptEngine}
 
 trait ScriptingEngine {
-  protected val scriptEngineManager: ScriptEngineManager
+  protected val scriptEngineManager = new ScriptEngineManager()
   protected val langEngine: ScriptEngine
   val engine = langEngine.asInstanceOf[ScriptEngine with Invocable]
 }
